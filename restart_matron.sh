2775af78b4abff83616b90bcb553d32d7b3aa0fe
Zack Scholl <zack.scholl@gmail.com>
2022-08-23 23:27:32 +0000

    fixes

--- restart_matron.sh
+++ restart_matron.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 kill -1 $(ps aux | grep "matron" | grep build | grep norns | grep -v grep | grep -v restart_matron | awk '{print $2}')
-nohup /home/we/norns/build/ws-wrapper/ws-wrapper ws://*:5555 /home/we/norns/build/matron/matron > /dev/null 2>&1 &
+DISPLAY=:0 nohup /home/we/norns/build/ws-wrapper/ws-wrapper ws://*:5555 /home/we/norns/build/matron/matron > /tmp/matron.log &2>1 &
 
--- restart_sclang.sh
+++ restart_sclang.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 kill -1 $(ps aux | grep "scsynth\|sclang" | grep -v grep | grep -v restart_sclang | awk '{print $2}')
-nohup /home/we/norns/build/ws-wrapper/ws-wrapper ws://*:5556 /usr/local/bin/sclang -i maiden > /dev/null 2>&1 &
+nohup /home/we/norns/build/ws-wrapper/ws-wrapper ws://*:5556 /usr/local/bin/sclang -i maiden > /tmp/sclang.log 2>&1 &
 
