a880e779d265720a9a597700bbf73201cf6b90a2
Zack Scholl <zack.scholl@gmail.com>
2022-10-11 18:52:47 -0700

    fix start norns

--- start_norns.sh
+++ start_norns.sh
@@ -1,9 +1,11 @@
 #/bin/bash
 
+export DISPLAY=:0
 export JACK_NO_START_SERVER=1
 export JACK_NO_AUDIO_RESERVATION=1
-export DISPLAY=:0
 
+sudo /etc/init.d/dbus start
+sudo chown -R we:we /home/we/dust
 Xvfb :0 -screen 0 1280x640x16 -fbdir /tmp &
 sleep 1
 cd /home/we/ && LOGGER=info /usr/local/go/bin/go run oled-server.go -window-name 'matron' -port 8889 &
@@ -12,9 +14,12 @@ jackd -V
 $(cat /etc/jackdrc) &
 sleep 1
 /home/we/norns/build/crone/crone &
+cd /home/we/norns/sc
 sleep 1
+timeout 0.5 /home/we/norns/build/ws-wrapper/ws-wrapper ws://*:5556 /usr/local/bin/sclang -i maiden
+sleep 0.5
 /home/we/norns/build/ws-wrapper/ws-wrapper ws://*:5556 /usr/local/bin/sclang -i maiden &
-sleep 2
+sleep 1
 /home/we/norns/build/ws-wrapper/ws-wrapper ws://*:5555 /home/we/norns/build/matron/matron &
 sleep 1
 cd /home/we/maiden && ./maiden server --app ./app/build --data ~/dust --doc ~/norns/doc &
